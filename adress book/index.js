97489bfeccfb3eec3fb724e38ca0d489357dd40c
Martaalex <40301963+Martaalex@users.noreply.github.com>
2018-11-28 22:04:42 +0200

    Add files via upload

---
+++ adress book/index.js
@@ -0,0 +1,7 @@
+const add = document.getElementById('addContact');
+const Name = document.getElementById('name');
+const phone = document.getElementById('phone');
+
+add.addEventListener("submit", addContact =>{
+   
+});
